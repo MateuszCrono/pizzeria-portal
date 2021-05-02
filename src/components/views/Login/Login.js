7c1c4bd2ecb589111082d8ff268403e53a3775f1
Mateusz Olszewski <matcrono@gmail.com>
2021-05-02 19:12:40 +0200

    fixed route pathing names

--- src/components/views/Login/Login.js
+++ src/components/views/Login/Login.js
@@ -6,9 +6,6 @@ const Login = () => (
 
   <div className={styles.component}>
     <Link exact to={`${process.env.PUBLIC_URL}/`} activeClassName='active'>Dashboard</Link>
-    <Link to={`${process.env.PUBLIC_URL}/tables`} activeClassName='active'>Tables</Link>
-    <Link to={`${process.env.PUBLIC_URL}/waiter`} activeClassName='active'>Waiter</Link>
-    <Link to={`${process.env.PUBLIC_URL}/kitchen`} activeClassName='active'>Kitchen</Link>
     <h2> {'Login View'}</h2>
   </div>
 
--- src/components/views/Waiter/Waiter.js
+++ src/components/views/Waiter/Waiter.js
@@ -5,10 +5,8 @@ import {Link} from 'react-dom';
 const Waiter = () => (
 
   <div className={styles.component}>
-    <Link exact to={`${process.env.PUBLIC_URL}/`} activeClassName='active'>Dashboard</Link>
-    <Link to={`${process.env.PUBLIC_URL}/login`} activeClassName='active'>Login</Link>
-    <Link to={`${process.env.PUBLIC_URL}/tables`} activeClassName='active'>Tables</Link>
-    <Link to={`${process.env.PUBLIC_URL}/kitchen`} activeClassName='active'>Kitchen</Link>
+    <Link to={`${process.env.PUBLIC_URL}/waiter/order/new`} activeClassName='active'>New Order</Link>
+    <Link to={`${process.env.PUBLIC_URL}/waiter/order/:id`} activeClassName='active'>Orders</Link>
     <h2> {'Waiter View'}</h2>
   </div>
 
