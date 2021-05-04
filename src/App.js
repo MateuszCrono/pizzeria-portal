8699882fc50802ebbfa4e56184c9d57839c8070e
Mateusz Olszewski <matcrono@gmail.com>
2021-05-04 17:35:59 +0200

    fixed kitchen routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import MainLayout from './components/layout/MainLayout/MainLayout.js';
-import Ktichen from './components/views/Kitchen/Kitchen/kitchen.js';
+import Kitchen from './components/views/Kitchen/kitchen.js';
 import Login from './components/views/Login/Login.js';
 import Tables from './components/views/Tables/Tables.js';
 import Waiter from './components/views/Waiter/Waiter.js';
@@ -32,7 +32,7 @@ function App() {
               <Route exact path={process.env.PUBLIC_URL + '/login'} component={Login} />
               <Route exact path={process.env.PUBLIC_URL + '/tables'} component={Tables} />
               <Route exact path={process.env.PUBLIC_URL + '/waiter'} component={Waiter} />
-              <Route exact path={process.env.PUBLIC_URL + '/kitchen'} component={Ktichen} />
+              <Route exact path={process.env.PUBLIC_URL + '/kitchen'} component={Kitchen} />
             </Switch>
           </MainLayout>
         </ThemeProvider>
